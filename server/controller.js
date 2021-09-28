7006438bd2985a9d7fbd9f34a08f0f1ac28f1dca
Chris Vombaur <89802742+Cvombaur18@users.noreply.github.com>
2021-09-28 15:42:50 -0600

    Add files via upload

---
+++ server/controller.js
@@ -0,0 +1,57 @@
+const houses = require('./db.json');
+
+let id = 4;
+
+module.exports = {
+    getHouses: (req, res) => {
+        res.status(200).send(houses)
+    },
+    deleteHouse: (req, res) => {
+        const { houseId } = req.params;
+
+        const tgtIndex = houses.findIndex(function(houseObj) {
+            return houseObj.id === parseInt(houseId);
+        })
+
+        if (tgtIndex === -1) {
+            res.status(404).send('House not found!')
+        } else {
+            houses.splice(tgtIndex, 1);
+            res.status(200).send(houses);
+        }
+    },
+    createHouse: (req, res) => {
+        const { address, imageURL, price } = req.body;
+
+        const newHouse = {
+            id,
+            address,
+            imageURL,
+            price
+        }
+
+        houses.push(newHouse);
+        id++;
+
+        res.status(200).send(houses);
+
+    },
+    updateHouse: (req, res) => {
+        const { houseId } = req.params;
+        const { type } = req.body;
+        const tgtIndex = houses.findIndex(function(houseObj) {
+            return houseObj.id === parseInt(houseId);
+        })
+        let tgtHouseObj = houses[tgtIndex];
+
+        if (type === 'plus') {
+            tgtHouseObj.price += 10000
+            res.status(200).send(houses);
+        }else if (type === 'minus') {
+            tgtHouseObj.price -= 10000
+            res.status(200).send(houses);
+        }else{
+            res.status(400).send('Something went wrong') 
+        }
+    }
+}
